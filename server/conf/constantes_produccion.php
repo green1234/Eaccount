7369633a0523b6885012e9d353c0dd317eddf926
mcgalv <mcgalv@gmail.com>
2015-01-28 16:08:28 -0600

    gitignore server/config/*

--- server/conf/constantes_produccion.php
+++
@@ -1,7 +0,0 @@
-<?php	
-	// define("HOST", "http://104.236.124.45");
-	define("NAME", "/eaccount/server");
-	// define("PROYECT_PATH", HOST . NAME);
-	define("PROYECT_PATH", $_SERVER["DOCUMENT_ROOT"] . NAME);
-
-?>
