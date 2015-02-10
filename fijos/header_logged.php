78cda8702e5ae6d777742ea7fec1cfa351f4ee2f
mcgalv <mcgalv@gmail.com>
2015-02-10 14:38:21 -0600

    Image Header Logged Updated

--- fijos/header_logged.php
+++ fijos/header_logged.php
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" onclick="getRecientes();">
-        <img width="130px" height="80px" src="img/logo_contabilidad_blanco.png" class="saavyLogo" style="max-width:85%">
+        <img width="130px" height="80px" src="img/logo_header_blanco.png" class="saavyLogo" style="max-width:85%">
       </a>
     </div>
     <div class="navbar-collapse collapse">
