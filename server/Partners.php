8c87cc91d90b00366bb3d0e1397a2f50f9319ce2
mcgalv <mcgalv@gmail.com>
2015-03-11 11:40:29 -0600

    Method for create bank account

--- server/Partners.php
+++ server/Partners.php
@@ -1,5 +1,6 @@
 <?php
 	
+	session_start();
 	require_once "conf/constantes.conf";
 	require_once PROYECT_PATH . "/service/PartnerService.php";
 	require_once PROYECT_PATH . "/service/InvoiceService.php";
@@ -16,6 +17,24 @@
 		// if ($res["success"])			
 		// 	logg($res["data"],1);
 		echo json_encode($res);		
+	}
+	else if ($_GET["cuentaban"] && isset($_SESSION["login"]))
+	{
+		$cid = $_SESSION["login"]["cid"][0];
+		
+		if ($_GET["cuentaban"] == "crear")
+		{			
+			$model = "res.company";
+			$method = "registrar_cuenta_bancaria";
+			$params = array(
+				"cid" => model($cid, "int"),
+				"cuenta" => model($_GET["cta_numero"], "string"),
+				"banco" => model($_GET["cta_banco"], "int"),);
+
+			$obj = new MainObject();
+			$res = $obj->call(USER_ID, md5(PASS), $model, $method, null, $params);	
+			echo json_encode($res);	
+		}
 	}
 
 	
