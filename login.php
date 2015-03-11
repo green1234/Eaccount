26b6632814efac4ffc1feec66ba4042f366cf98a
Gerardo A Lopez Vega <mcgalv@gmail.com>
2015-03-11 14:03:57 -0600

    Change fijos for view on reference for footer

--- login.php
+++ login.php
@@ -116,7 +116,7 @@
     </div>
 
     <footer>
-        <? require 'fijos/footer.php'; ?>
+        <? require 'view/footer.php'; ?>
     </footer>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
