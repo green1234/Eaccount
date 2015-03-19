6f12840eddcf2443912755e68b13f76e73fb7ae8
Gerardo A Lopez Vega <mcgalv@gmail.com>
2015-03-19 17:13:04 -0600

    Remove Example Code

--- view/config/catalogos/clientes.php
+++ view/config/catalogos/clientes.php
@@ -19,40 +19,7 @@
       <th>
         <b>Cuenta Bancaria: <img src="img/interrogacion.png" whidth="15" height="15" class="popover-1" title="Cuenta de pago del Cliente"></b>
       </th>
-    </tr>
-    <?//INICIA FOR DE LOS DATOS?>
-
-    <tr>
-    	<td>
-        <input type="radio">
-      </td>
-      <td>
-        102
-      </td>
-      <td>
-        Martha Angela Martinez Flores
-      </td>
-      <td>
-        PMAMF
-      </td>
-      <td>
-        <select>
-          <option value="0">0</option>
-          <option value="7">7</option>
-          <option value="15">15</option>
-          <option value="30">30</option>
-          <option value="45">45</option>
-          <option value="60">60</option>
-          <option value="90">90</option>
-          <option value="120">120</option>
-          <option value="180">180</option>
-          <option value="Otro">Otro</option>
-        </select>
-      </td>
-      <td>
-        BMX *5494
-      </td>
-    </tr>
+    </tr>    
 
     <?//TERMINA FOR DE LOS DATOS?>
   </table>
